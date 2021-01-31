bad0e7a64893e4ed47068fe39b1fc0aa7e0a21fb
Chrisphine10 <ondiekchrisphine@gmail.com>
2021-01-31 13:54:45 +0300

    all done for production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
   skip_before_action :authorize
+  ACCOUNTS_SIZE = 4
+  OPPS_SIZE = 10
   def index
   end
   def show
-    @accounts = Account.paginate page: params[:page], per_page: 4
-    @opportunities = Opportunity.paginate page: params[:page], per_page: 10
+    @page = (params[:page] || 0).to_i
+    @sage = (params[:sage] || 0).to_i
+    @accounts = Account.offset(ACCOUNTS_SIZE * @page).limit(ACCOUNTS_SIZE)
+    @opportunities = Opportunity.offset(OPPS_SIZE * @sage).limit(OPPS_SIZE)
   end
+
 end
