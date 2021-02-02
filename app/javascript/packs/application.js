02f11d6f0be0c0b08453ad562d8d032a0771ac7f
Chrisphine10 <ondiekchrisphine@gmail.com>
2021-02-02 11:44:50 +0300

    finally

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 
 import "bootstrap"
 import "../stylesheets/application"
+import "../stylesheets/applications"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
